1b83da4d6981c2405622222e14b63a9cbcf1357c
otsee <76693678+otseeserver@users.noreply.github.com>
2021-03-20 08:57:51 +0700

    Update telstr.py

--- telstr.py
+++ telstr.py
@@ -14,6 +14,7 @@ By: <a href='tg://user?id=1285496612'>『ᴏᴛsᴇᴇ』</a>"""
 with TelegramClient(StringSession(), API_ID, API_HASH) as client:
     str = client.session.save()
     saved_messages_template = template.format(str)
+    print("\nGeneratimg Strung Session...\n")
     client.send_message("me", saved_messages_template, parse_mode="html")
     print("Your String Session have been send to your Telgram Saved Messages")
 
